e8b16b4b0fb55c10d16c2a6f724c5ec13406e1d2
sauravchauhan2 <saurav.chauhan@egreycell.com>
2024-02-19 12:57:10 +0530

    Manage all comments

---
+++ handling of async/main.js/main.js
@@ -0,0 +1,20 @@
+//How can you optimize the handling of async data promises in the below code?
+
+import { useCallback, useEffect } from "react";
+ function TestComponent(props) {
+  const fetchData = useCallback(async () => {
+	const response = await fetch(`/api/data/${props.id}`);
+	const json = await response.json();
+	return json;
+  }, [props.id]);
+ 
+  useEffect(() => {
+    const dataPromise = fetchData();
+
+	return () => {
+	};
+  }, [fetchData]);
+   return <div>My Component</div>;
+}
+
+//Adding All My Data dsdsjkgh dhdkdjfdsj jfkhdsjkfhsdjk
